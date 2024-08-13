b8eb6e863e4f0f723e0614a8995dbfc0c1c8a348
suraj <okaybui22@gmail.com>
2024-08-13 13:46:05 +0530

    Update browser tab name and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import juices from "../assets/juices.png";
 
 const Header = () => {
   return (
-    <div className="  w-full  px-5 py-3 shadow-lg border-b-2 bg-gradient-to-t from-orange-400 to-yellow-200  sticky top-0">
+    <div className="  w-full  px-5 py-3 shadow-lg  bg-gradient-to-t from-orange-400 to-yellow-200  sticky top-0">
       <div className="flex items-center justify-between">
         <div>
           <GiHamburgerMenu size={35} className="" />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,20 +10,26 @@ const Home = () => {
   return (
     <div className="w-full h-auto min-h-screen bg-gradient-to-b from-gray-300 to-gray-200 ">
       <Header />
-      <div className=" p-5">
-        <div className="text-center py-8">
-          <b className="text-5xl font-amatic text-gray-800">Go Healthy</b>
-        </div>
 
-        <div className="m-auto">
-          <div className="grid grid-cols-4 gap-4">
-            <CategoryBtn item={items} setmenu={setMenu} />
-          </div>
+      <div className="fixed w-full px-5 backdrop-blur-md  ">
+        <div className="text-center  my-5">
+          <span className="text-5xl font-semibold font-amatic text-gray-800">
+            Go Healthy
+          </span>
         </div>
-
-        <div className="space-y-1.5 pt-8">
+        <div className="grid grid-cols-4 gap-4 py-2 ">
+          <CategoryBtn item={items} setmenu={setMenu} />
+        </div>
+      </div>
+      <div className="pb-5 px-5 mt-40 ">
+        <div className="space-y-1.5 ">
           {menu.map((data, index) => (
-            <ItemCard key={index} name={data.name} price={data.price} img={data.img}/>
+            <ItemCard
+              key={index}
+              name={data.name}
+              price={data.price}
+              img={data.img}
+            />
           ))}
         </div>
       </div>
